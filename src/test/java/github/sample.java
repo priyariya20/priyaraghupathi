e2ce2787b06680e00fd9f0415dc86d0349a7cf2e
priyariya20 <priyariya.vlr97@gmail.com>
2021-10-21 20:19:55 +0530

    project structure

--- src/test/java/github/sample.java
+++ src/test/java/github/sample.java
@@ -2,7 +2,8 @@ package github;
 
 public class sample {
 	public static void main(String[] args) {
-		
+		System.out.println("he was playing");
+		System.out.println("ground was so nasty");
 	}
 
 }
